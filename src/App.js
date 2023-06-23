f48b699214f67a4d5ba0b618035cce520901465b
Suziech <choisk09@gmail.com>
2023-06-23 17:46:47 +0900

    completed todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background: #e9ecef;
+}`;
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
